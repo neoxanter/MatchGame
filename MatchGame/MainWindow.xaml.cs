b3080e64b536226e349e8850cfebd99fe16f045e
neoxanter <sgcont@gmail.com>
2024-05-27 20:24:53 +0500

    Added game reset if all 8 elements are found

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -86,5 +86,13 @@ namespace MatchGame
                 findingMatch = false;
             }
         }
+
+        private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
+        }
     }
 }
